c24498f1d1f52992eec5161ee9caf9f2629cdf8a
sojpark <soparker@naver.com>
2020-05-28 07:59:35 +0900

    retry01

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | grep -v "#" | awk 'NR % 2 == 0' | awk -F ':' '{print $1}' | rev | sort -r | sed -n '7,15 p' | tr '\n' ',' | sed 's/,$/\./' | sed 's/,/, /g' | tr -d '\n'
+cat /etc/passwd | grep -v "#" | awk 'NR % 2 == 0' | awk -F ':' '{print $1}' | rev | sort -r | sed -n "$FT_LINE1 , $FT_LINE2 p" | tr '\n' ',' | sed 's/,$/\./' | sed 's/,/, /g' | tr -d '\n'
